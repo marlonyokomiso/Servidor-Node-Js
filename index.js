297f4264059d8c228b132f723c710fcff168e5d7
MailoDev <75257397+MailoDev@users.noreply.github.com>
2021-01-30 12:57:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const express = require('express');
 const app = express();
 const { join } = require('path')
 
-app.get('/', (request, response) => {
-    esponse.sendFile(join(__dirname, './index.html'));
+app.get('/', (_req, res) => {
+    res.sendFile(join(__dirname, './index.html'));
 });
 
 app.get('/exemplo', (_req, res) => {
-    res.sendFile(join(__dirname, 'teste.html'))
+    res.sendFile(join(__dirname, 'exemplo.html'))
 });
 
-app.listen(3333); // Coloque a porta que quiser, mas a porta tem que conter 4 digitos em numeros!
+app.listen(4000); // Coloque a porta que quiser, mas a porta tem que conter 4 digitos em numeros!
